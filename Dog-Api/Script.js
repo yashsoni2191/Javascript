abbd03def37ee255984b90fb186d28f8f97c1dba
yashsoni2191 <yashsoni2191@gmail.com>
2024-10-14 15:35:38 +0530

    Javascript

--- Dog-Api/Script.js
+++ Dog-Api/Script.js
@@ -1,4 +1,4 @@
-const dogGallery = document.getElementById('dogGallery');
+const dogGallery = document.getElementById('dogGallery')
 
 function fetchDogs() {
     fetch('https://dog.ceo/api/breeds/image/random')
@@ -8,7 +8,7 @@ function fetchDogs() {
                 <div class="dog-card">
                     <img src="${data.message}" alt="Random Dog">
                 </div>
-            `;
+            `
         })
-        .catch(error => console.error("Error fetching dog image:", error));
+        .catch(error => console.error("Error fetching dog image:", error))
 }
