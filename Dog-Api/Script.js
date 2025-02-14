278d5cf1a7b4404da247ccfe6359edc8b0830f9c
yashsoni2191 <yashsoni2191@gmail.com>
2025-02-14 16:04:46 +0530

    Dog-API

--- Dog-Api/Script.js
+++ Dog-Api/Script.js
@@ -1,14 +1,15 @@
-const dogGallery = document.getElementById('dogGallery')
+document.getElementById("fetchBtn").onclick = function() {
+    fetchDogs();
+};
 
 function fetchDogs() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
-        .then(data => {
-            dogGallery.innerHTML = `
-                <div class="dog-card">
-                    <img src="${data.message}" alt="Random Dog">
-                </div>
-            `
-        })
-        .catch(error => console.error("Error fetching dog image:", error))
+    let gallery = document.getElementById("dogGallery");
+    gallery.innerHTML = ""; 
+
+    for (let i = 0; i < 4; i++) {
+        let img = document.createElement("img");
+        img.src = "https://placedog.net/400/300?random=" + Math.random();
+        img.alt = "Random Dog";
+        gallery.appendChild(img);
+    }
 }
